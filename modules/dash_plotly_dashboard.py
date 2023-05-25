fc5d380a6a52aa77336b2ec38bc04ca0e0c5d7d2
AlexandreGarito <alexandre.garito@gmail.com>
2023-05-25 14:49:50 +0200

    Fix scatter

--- modules/dash_plotly_dashboard.py
+++ modules/dash_plotly_dashboard.py
@@ -32,7 +32,7 @@ def dashboard():
     
     df["normalized_sentiment"] = scaler.fit_transform(df[["yest_twitter_mean_sentiment_score"]])
     df["normalized_sentiment"] = df["normalized_sentiment"].round(2)
-    df = df.fillna(df.mean())
+    df["normalized_sentiment"] = df["normalized_sentiment"].fillna(df["normalized_sentiment"].mean())
     
     external_stylesheets = [
         'https://fonts.googleapis.com/css2?family=Lato&display=swap',
